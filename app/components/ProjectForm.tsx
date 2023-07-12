02116d947a9dc19ce681492600b35dd8b294012a
FelipeMDantas <felipemdantas@terra.com.br>
2023-07-11 22:08:46 -0300

    Modal component further developed.

--- app/components/ProjectForm.tsx
+++ app/components/ProjectForm.tsx
@@ -1,5 +1,14 @@
-const ProjectForm = () => {
-  return <div>ProjectForm</div>;
+import { SessionInterface } from "@/common.types";
+
+type Props = {
+  type: string;
+  session: SessionInterface;
+};
+
+const ProjectForm = ({ type, session }: Props) => {
+  const handleFormSubmit = (e: React.FormEvent) => {};
+
+  return <form onSubmit={handleFormSubmit} className="flexStart form"></form>;
 };
 
 export default ProjectForm;
